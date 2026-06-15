35768ebe5fb04368c54f6d43a9a9f884ca0e6aca
stickiebomb <originalstarwalkerr@gmail.com>
2026-06-15 12:59:54 -0700

    Change application icon to 'niko.ico'

--- cwmain.py
+++ cwmain.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("cw v0.02")
 
 # icon
-root.iconbitmap('pfp (1).ico')
+root.iconbitmap('niko.ico')
 
 # add text
 label = tk.Label(root, text="Oops! You're here early! I'm still working on this.")
